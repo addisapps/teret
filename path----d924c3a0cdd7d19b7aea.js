08e2a485e4a7f4f1941960554891b6eb59089a22
Elias Muluneh <eliasmu@gmail.com>
2018-06-02 13:04:38 -0400

    Updates

---
+++ path----d924c3a0cdd7d19b7aea.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{102:function(t,a){t.exports={data:{site:{siteMetadata:{title:"ተረት አና ግጥሞች"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----d924c3a0cdd7d19b7aea.js.map
